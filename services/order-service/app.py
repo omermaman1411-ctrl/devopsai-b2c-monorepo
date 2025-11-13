c2ca11e64d5eee76780810493bdd89eb6dc8c67d
baruchih10 <bhalamish10@gmail.com>
2025-11-13 14:32:36 +0200

    noam test

--- services/order-service/app.py
+++ services/order-service/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from itsdangerous import URLSafeSerializer
 from decimal import Decimal, ROUND_HALF_UP
 import os
-import noam
+noam
 
 app = Flask(__name__)
 SECRET = os.getenv("SECRET_KEY","dev-secret")
